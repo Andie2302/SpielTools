247e4056e45193621a71666cffe7242aaf618632
andreas <Andreas.Zwander@gmail.com>
2026-01-30 15:55:43 +0100

    Remove `Entity` and `World` classes from `SpielTools` and clean up unused imports

---
+++ SpielTools/GameEngine.cs
@@ -0,0 +1,19 @@
+﻿namespace SpielTools;
+
+
+public class GameEngine
+{
+    public float PhysicsDeltaTime { get; set; } = 0.01f;
+    private readonly World _world;
+
+    public GameEngine(World world)
+    {
+        _world = world;
+    }
+
+    public void DoStep()
+    {
+        // Die gesamte Magie passiert hier
+        _world.Update(PhysicsDeltaTime);
+    }
+}
