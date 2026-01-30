53dd25dae3949966c3c707ef879c33d08775b035
andreas <Andreas.Zwander@gmail.com>
2026-01-30 16:47:33 +0100

    Remove redundant comments from `GameAction` enum

--- SpielTools/GameAction.cs
+++ SpielTools/GameAction.cs
@@ -2,8 +2,8 @@ namespace SpielTools;
 
 public enum GameAction
 {
-    Idle = 0,       // Nichts tun / Warten
-    MoveLeft = 1,   // Nach Links laufen
-    MoveRight = 2,  // Nach Rechts laufen
-    Jump = 3        // Springen
+    Idle = 0,
+    MoveLeft = 1,
+    MoveRight = 2,
+    Jump = 3
 }
