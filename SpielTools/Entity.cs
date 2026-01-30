01a3c161f54e6637f0f4eedc15e446b32d8d3e44
andreas <Andreas.Zwander@gmail.com>
2026-01-30 16:47:29 +0100

    Simplify `Update` method in `Entity` by removing redundant comments

--- SpielTools/Entity.cs
+++ SpielTools/Entity.cs
@@ -9,10 +9,8 @@ public abstract class Entity
     public Vector2 Size { get; set; }
     public bool IsGrounded { get; protected set; }
 
-    // Jede Entity entscheidet selbst, wie sie auf die Zeit reagiert
     public virtual void Update(float deltaTime, World world)
     {
-        // Einfache Bewegung basierend auf der Geschwindigkeit
         Position += Velocity * deltaTime;
     }
 }
