de60dd1f9704e35458ceac7052654302e1dcd3c3
andreas <Andreas.Zwander@gmail.com>
2026-01-30 16:11:16 +0100

    Add `GameAction` enum with basic player actions in `SpielTools`

---
+++ SpielTools/GameAction.cs
@@ -0,0 +1,9 @@
+namespace SpielTools;
+
+public enum GameAction
+{
+    Idle = 0,       // Nichts tun / Warten
+    MoveLeft = 1,   // Nach Links laufen
+    MoveRight = 2,  // Nach Rechts laufen
+    Jump = 3        // Springen
+}
